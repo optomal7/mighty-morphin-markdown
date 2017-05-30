c4f89dd0e72687d74ed7720b7dbb93ddd168a50e
mikeadossi <mikeadossi@gmail.com>
2017-05-30 15:19:54 -0700

    remove header text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mighty Morphin Markdown' });
+  res.render('index', { title: 'mighty morphin markdown' });
 });
 
 module.exports = router;
