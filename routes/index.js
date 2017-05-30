00ca2d278c9338706bc0571d6de61eeaf35056a3
mikeadossi <mikeadossi@gmail.com>
2017-05-30 15:17:15 -0700

    style index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mighty Morphin Markdown' });
 });
 
 module.exports = router;
