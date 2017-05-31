40f2f56b05af2fdc8f32bf9245d5896ad37b1ba4
Ryan Greenberg <hawk1138@gmail.com>
2017-05-31 12:32:21 -0700

    working on title+navebar+save

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,18 @@ $(document).ready(function(){
   $('.nav_plus').on('click', function(){
     console.log("click");
     var title = "untitled";
-    title = prompt("Enter filename");
+    title = prompt("Enter filename") + ".md";
+    console.log(title);
+
+    $(".file_name").html(title);
+
+
+  })
+
+  $("#sv_file").on('click', function(){
+    console.log("heyoo!");
+    var title = $(".file_name").html();
+    console.log(title);
+    $('.nav_link_container').prepend('<div class="nav_link" id="title_'+title+'">'+title+'</div><i id="trash_'+title+'" aria-hidden="true" class="fa fa-trash-o"></i>')
   })
 });
