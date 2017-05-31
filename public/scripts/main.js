5600df0e747fc16ce9aa692927e0bcb775e24882
Ryan Greenberg <hawk1138@gmail.com>
2017-05-30 18:07:01 -0700

    buttons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,9 @@
 console.log(marked('I am using __markdown__!'));
+
+$(document).ready(function(){
+  $('.nav_plus').on('click', function(){
+    console.log("click");
+    var title = "untitled";
+    title = prompt("Enter filename");
+  })
+});
