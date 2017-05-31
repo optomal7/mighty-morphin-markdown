ea4751d0315eb5a6fe0e9680aaedd0b4021769e1
Ryan Greenberg <hawk1138@gmail.com>
2017-05-31 14:42:49 -0700

    trash btn hides content

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,16 +16,25 @@ $(document).ready(function(){
   $("#sv_file").on('click', function(){
     console.log("heyoo!");
     var title = $(".file_name").html();
+    title = title.substring(0, title.length - 3 );
     console.log(title);
-    $('.nav_link_container').prepend('<div class="nav_link" id="title_'+title+'">'+title+'</div><i id="trash_'+title+'" aria-hidden="true" class="fa fa-trash-o"></i>')
+    $('.nav_link_container').prepend('<div class="nav_link name-'+title+'" data-id="'+title+'">'+title+'</div><i data-id="'+title+'" aria-hidden="true" class="fa fa-trash-o delete-'+title+'"></i>')
   })
+
+  $(document).on('click', '.fa-trash-o',
+  function () {
+      var id = $(this).data('id')
+      console.log('name-'+id)
+      $('.name-'+id).hide()
+      $('.delete-'+id).hide()
+    }
+  )
 
   $( '.markdown_textarea' ).keyup(function() {
     var value = marked( $( this ).val() );
     $( '.preview_textarea' ).html( value );
   }).keyup();
 
-HTMLView.innerHTML = marked(markdownview.textContent)
-
+  HTMLView.innerHTML = marked(markdownview.textContent)
 
 });
